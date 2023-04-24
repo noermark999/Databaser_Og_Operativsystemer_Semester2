3f3a7e608780fa3e8a95e0bfa69a6181ccb4be70
Noermark999 <jakobnoermark@hotmail.com>
2023-04-24 11:40:43 +0200

    Lek13

---
+++ Lek13_Threads/src/traadeks22T/TestSnakkesagligPerson.java
@@ -0,0 +1,26 @@
+package traadeks22T;
+
+public class TestSnakkesagligPerson {
+
+	public static void main(String[] args) {
+		SnakkesagligPerson ulla = new SnakkesagligPerson("Ulla", 20);
+		SnakkesagligPerson pia = new SnakkesagligPerson("Pia", 10);
+		SnakkesagligPerson ib = new SnakkesagligPerson("Ib", 30);
+
+		//man bruger implements Runnable skal man først lave en tråd på Runnable objektet
+		Thread tradUlla = new Thread(ulla);
+		Thread tradPia = new Thread(pia);
+		Thread tradIb = new Thread(ib);
+		
+		tradUlla.start();
+		tradPia.start();
+		tradIb.start();
+
+//bruges når man extends Thread
+//		ulla.start();
+//		pia.start();
+//		ib.start();
+
+	}
+
+}
